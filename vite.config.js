b0b3f4941370c42370963214ebaadb359a150f85
xinyew <xinyew@gatech.edu>
2026-06-28 20:12:17 -0400

    fixed stale-path bug

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      // Allow Vite to statically serve images directly from your www.oma.com data dump
-      allow: ['/Users/xinye/Downloads/']
-    }
-  }
 })
